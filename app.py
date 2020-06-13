013610d0f850f5abc944c5721590edbf8a302af0
real-habibul <ahmadhabibululum@gmail.com>
2020-06-13 16:21:55 +0700

    memperbaiki pesan response

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ class ContohResource(Resource):
         umur = request.form["umur"]
         identitas["nama"] = nama
         identitas["umur"] = umur
-        response = {"msg": "Data berhasil dimasukkan!"}
+        response = {"msg": "Sukses BOSQUE!"}
         return response
 
 
